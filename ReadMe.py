9f3c92b8741153a37d64b2cdde57a8c832795e99
renfei-Zhou <tobyzrf1997@gmail.com>
2026-07-08 21:40:39 +0200

    update readme

--- ReadMe.py
+++ ReadMe.py
@@ -8,3 +8,13 @@ echo "source /opt/ros/jazzy/setup.bash" >> ~/.bashrc
 echo "source ~/Desktop/ROS2_WS/install/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 '''
+
+# Create a new package
+'''
+ros2 pkg create PACKAGE_NAME --build-type ament_python --dependencies rclpy --node-name NODE_NAME
+'''
+
+# Run a node in terminal
+'''
+ros2 run PACKAGE_NAME NODE_NAME
+'''
